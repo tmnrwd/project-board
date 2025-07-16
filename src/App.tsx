d7fc0c7ddeaea19472dad83cd66cf6deffe399ab
Tim Norwood <tmvnorwood@gmail.com>
2025-07-16 23:18:19 +0100

    icon deploy path update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <main className="flex flex-col">
       <header className="flex h-24 bg-light-green">
-        <img src="/logo.png" height={64} className="h-16 m-auto" />
+        <img src="logo.png" height={64} className="h-16 m-auto" />
       </header>
     </main>
   );
